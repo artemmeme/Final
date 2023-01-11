afa11970bdea271d0819181544ea583330b771c0
Артем Шустов <artemmeme2017@yandex.ru>
2023-01-11 15:58:41 +0300

    create_processing_function

--- final/Program.cs
+++ final/Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array_first = new string[8] {"321", "89", "hello", "world", "computer", "167", "1875", "13"};
 string[] array_second = new string[array_first.Length];
+void Processing(string[] array1, string[] array_second)
+{
+    int k = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array_second[k] = array1[i];
+        k++;
+        }
+    }
+}
